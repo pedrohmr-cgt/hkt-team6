1878209d892fdedadf93ccff0a3d82acfe2a511c
Pedro Rodrigues <pedro.henrique@cgtrader.com>
2021-10-06 09:25:37 -0300

    Split the App in components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './assets/logo.svg';
-import './App.css';
+
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Layout>
+      <Home />
+    </Layout>
   );
 }
 
